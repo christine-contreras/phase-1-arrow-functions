b6c0ff27aa8e43cc2024cc370a7408b1be479939
Christine Contreras <christineec23@yahoo.com>
2021-04-07 13:29:55 -0500

    completed task

---
+++ index.js
@@ -0,0 +1,16 @@
+
+//function expression called divide
+const divide = function() {
+    //divides 2000 by 100
+    return 2000 / 100;
+  }
+
+
+//arrow function square
+//square arrow function takes one parameter and multiplies it times itself
+const square = x => x *= x;
+
+
+//arrow function add
+//add arrow function takes two parameters and adds them together
+const add = (a,b) => a + b;
